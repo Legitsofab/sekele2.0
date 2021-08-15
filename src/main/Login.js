58e0afd3eef20df714790f95bf6bd7144755e39a
Legitsofab <sahputraakbar123@gmail.com>
2021-08-15 15:37:47 +0700

    Login Page #1

--- src/main/Login.js
+++ src/main/Login.js
@@ -1,7 +1,32 @@
+import Nav from './sub/LogoutNav'
+import Footer from './sub/Footer'
+
 const Login = () => {
     return (
         <div className="login-wrapper">
-            <h2>Ini Login Page</h2>
+            <div className="bg-purple-200">
+                <Nav />
+                <div className="content-wrapper">
+                    <div className="text-5xl text-purple-500 font-bold text-center py-10">
+                            LOGIN
+                    </div>
+                    <div className="flex flex-col justify-center items-center">
+                        <form action="POST">
+                            <label htmlFor="email" className="text-purple-500 font-bold">Email</label>
+                            <br />
+                            <input type="text" id="email" className="rounded-md px-2 font-sans"/>
+                            <br />
+                            <label htmlFor="password" className="text-purple-500 font-bold">Password</label>
+                            <br />
+                            <input type="password" id="password" className="rounded-md px-2 font-sans"/>
+                            <br />
+                            <br />
+                            <button type="submit" className="bg-purple-500 rounded-md py-2 px-2 m-auto text-white font-bold w-full">Login</button>
+                        </form>
+                    </div>
+                </div>
+                <Footer />
+            </div>
         </div>
     );
 }
